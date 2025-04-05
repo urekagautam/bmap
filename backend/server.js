632476fb636ba9d3d68813f5740cac0ea77240b7
urekagautam <urekagautam19@gmail.com>
2025-04-05 10:29:12 +0545

    added some packages

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 app.get('/', (req,res)=>{
     res.send('Server is ready!');
@@ -9,5 +10,5 @@ app.get('/', (req,res)=>{
 const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
-    console.log(`Serve at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
