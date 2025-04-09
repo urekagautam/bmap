db994ff5bfbcf963b53ce2521764ee5067e98f48
urekagautam <urekagautam19@gmail.com>
2025-04-09 20:41:58 +0545

    feat: added login and logout features. also, created middleware to verify JWT Token along with the cookies, headers stored,etc

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,10 +1,118 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import {ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 
-const registerUser = asyncHandler (async ( req, res ) => {
-  res.status(200).json({
-    message: "ok"
-  })
-}) 
+const genarateAccessAndRefreshTokens = async (userId) =>{
+try{
+const user = await User.findById(userId)
+const accessToken = user.genarateAccessToken
+const refreshToken = user.genarateRefreshToken
 
-export {registerUser}; 
+user.refreshToken = refreshToken
+await user.save({ validateBeforeSave: false})
+
+return {accessToken, refreshToken}
+
+}catch(error){
+  return next(new ApiError(500, "Something went wrong while genarating refresh and access tokens"));
+}
+}
+
+const registerUser = asyncHandler(async (req, res, next) => {
+  //get user details front frontend
+  const { firstname, lastname, email, password, confirmPassword } = req.body;
+
+  if (!firstname || !lastname || !email || !password || !confirmPassword) {
+    return next(new ApiError(400, "All fields are required"));
+  }
+
+  if (await User.findOne({ email })) {
+    return next(new ApiError(400, "Email is already used"));
+  }
+
+  const user = await User.create({firstname, lastname, email, password});
+
+  const createdUser = await User.findById(user._id).select("-password -refreshToken")
+  if(!createdUser){
+  return next(new ApiError(500, "Something went wrong while regsitering the user!"));
+  }
+
+  return res
+  .status(201)
+  .json(
+    new ApiResponse(
+      200, 
+      createdUser, 
+      "User Registered Successfully")
+  )
+});
+
+const loginUser = asyncHandler(async (req, res, next) => {
+  const {email, password} = req.body;
+
+  if(!email || !password){
+    return next(new ApiError(400, "All fields are required"));
+  }
+
+  const user = User.findOne({email});
+  if(!user){
+    return next(new ApiError(400, "User doesnt exist"));
+  }
+  
+  const isPasswordCorrect = await user.isPasswordCorrect(password);
+  if(!isPasswordCorrect){
+    return next(new ApiError(401, "Incorrect Password"));
+  }
+
+  const{accessToken, refreshToken} = await genarateAccessAndRefreshTokens(user._id);
+  
+  //get logged in user
+  const loggedInUser = await User.findById(user._id)
+  .select("-password -refreshToken")
+
+  //sending cookies
+  const options = {
+    httpOnly: true,
+    secure: true
+  }
+
+  return res
+  .status(200)
+  .cookie("accessToken", accessToken, options)
+  .cookie("refreshToken", refreshToken, options)
+  .json(
+    new ApiResponse(200,
+      {
+        user : loggedInUser, accessToken,refreshToken
+      },
+      "User Logged In Successfully !"
+    )
+  )
+});
+
+const logoutUser = asyncHandler(async(req, res)=>{
+await User.findByIdAndUpdate(
+  req.user._id,
+  {
+    $set: {
+      refreshToken: undefined
+    }
+  },
+  {
+    new: true
+  },
+)
+
+const options = {
+  httpOnly: true,
+  secure: true
+}
+
+return res.status(200)
+.clearCookie("accessToken", options)
+.clearCookie("refreshToken", options)
+.json(new ApiResponse(200, {}, "User logged out successfully"))
+})
+
+export { registerUser, loginUser, logoutUser };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,12 @@
 import {Router} from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
-// router.route("/login").post(loginUser);
+router.route("/login").post(loginUser);
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router;
