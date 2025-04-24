b01261584066bd44082c4a699a95d3b08afa6062
urekagautam <urekagautam19@gmail.com>
2025-04-24 23:12:18 +0545

    created job card component and limited skills to be only 3 in vacancy card like in job card

--- frontend/src/component/VacancyCard.jsx
+++ frontend/src/component/VacancyCard.jsx
@@ -5,48 +5,50 @@ import { IconLightBulb } from "./icons/IconLightBulb.jsx";
 import styles from "./VacancyCard.module.css";
 import SkillsTag from "./SkillsTag.jsx";
 
-export default function VacancyCard({deadline,views,vacancyTitle,vacancyCompany,skills=[]}) {
-/*   const deadline = "1 week, 3 days from today";
-  const views = "500";
-  const vacancyTitle = "Primary Level Teacher";
-  const vacancyCompany = "Gita Mata School";
-  const skills = ["Communication", "Motivation", "Supervison"]; */
-
+export default function VacancyCard({
+  deadline,
+  views,
+  vacancyTitle,
+  vacancyCompany,
+  skills = [],
+}) {
   return (
     <div className={styles.mainWrapper}>
       <div className={styles.shrinkWrapper}>
-      <div className={styles.topMainWrapper}>
-        <div className={styles.topLWrapper}>
-           <img className={styles.loginimg} src="/RandomImage.png" alt="RandomImage" />
-        </div>
-        <div className={styles.topRWrapper}>
-          <h1>{vacancyTitle}</h1>
-          <h2>{vacancyCompany}</h2>
+        <div className={styles.topMainWrapper}>
+          <div className={styles.topLWrapper}>
+            <img
+              className={styles.loginimg}
+              src="/RandomImage.png"
+              alt="RandomImage"
+            />
+          </div>
+          <div className={styles.topRWrapper}>
+            <h1>{vacancyTitle}</h1>
+            <h2>{vacancyCompany}</h2>
 
-          <div className={styles.skillsWrapper}>
-            <IconLightBulb />
-            <h3>Key Skills:</h3>
-            {skills.map((skill, index) => (
-              <SkillsTag key={index} skill={skill} />
-            ))}
+            <div className={styles.skillsWrapper}>
+              <IconLightBulb />
+              <h3>Key Skills:</h3>
+              {skills.slice(0, 3).map((skill, index) => (
+                <SkillsTag key={index} skill={skill} />
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.bottomMainWrapper}>
+          <div className={styles.bottomLWrapper}>
+            <IconHourglass />
+            <h3>Apply Before: {deadline}</h3>
+          </div>
+
+          <div className={styles.bottomRWrapper}>
+            <IconEyeOpen />
+            <h3>{views}</h3>
           </div>
         </div>
       </div>
-
-      <div className={styles.bottomMainWrapper}>
-
-        <div className={styles.bottomLWrapper}>
-        <IconHourglass />
-        <h3>Apply Before: {deadline}</h3>
-        </div>
-
-        <div className={styles.bottomRWrapper}>
-        <IconEyeOpen />
-        <h3>{views}</h3>
-        </div>
-
-      </div>
-      </div>
     </div>
-  )
+  );
 }
