af7b6a5a835b5b776da97ebe8a39d32dd08140dc
urekagautam <urekagautam19@gmail.com>
2025-04-25 14:38:29 +0545

    separated company profile by tab groups

---
+++ frontend/src/features/Organization/profile/JobsTab.jsx
@@ -0,0 +1,9 @@
+import styles from "./JobsTab.module.css"
+
+export default function JobsTab() {
+  return (
+    <div className={styles.jobsTabSection}>
+    <h2>This is the Jobs tab</h2>
+  </div>
+  )
+}
