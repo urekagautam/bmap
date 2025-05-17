39f74b8a1c2f40d9944ab158663f0d6bf65bae26
Sudip Adhikari <sudip.adhk1@gmail.com>
2025-05-17 14:48:31 +0545

    Merge pull request #7 from urekagautam/branchureka
    cleaned up some files and added district constant making district as selectable field instead of an input field

--- backend/routes/organization.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import {registerOrganization, loginOrganization, logoutOrganization} from "../controllers/organization.contoller.js"
-
-const router = Router();
-
-router.route("/auth/signup").post(registerOrganization);
-router.route("/auth/login").post(loginOrganization);
-
-export default router;
--- backend/routes/vacancy.routes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import { createVacancy, getAllVacancies } from "../controllers/vacancy.controller.js";
-
-const router = Router();
-
-router.route("/createVacancy").post(createVacancy);
-router.route("/getAllVacancies").get(getAllVacancies);
-
-export default router;
