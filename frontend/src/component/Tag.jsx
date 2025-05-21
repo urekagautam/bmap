dc0b8a1e0c17132c0e1ec65f05b393a8115e984e
urekagautam <urekagautam19@gmail.com>
2025-05-21 11:09:53 +0545

    added icon and color props in tag

--- frontend/src/component/Tag.jsx
+++ frontend/src/component/Tag.jsx
@@ -1,10 +1,12 @@
+import { cns } from '../utils/classNames'
 import styles from './Tag.module.css'
 
-export default function Tag({skill}) {
+export default function Tag({data, icon=null, layout="default"}) {
  
   return (
-    <div className={styles.tagWrapper}>
-      <h2>{skill}</h2>
+    <div className={cns(styles.tagWrapper, styles[layout])}>
+      {icon}
+      <h2>{data}</h2>
     </div>
   )
 }
--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -1,25 +1,16 @@
-import JobCard from "../component/JobCard.jsx";
 
+import Tag from "../component/Tag.jsx";
 import styles from "./Test.module.css";
+import {IconEyeOpen} from "../component/icons/IconEyeOpen.jsx"
+import {IconBills} from "../component/icons/IconBills.jsx"
+
 
 export default function Test() {
   return (
     <div className={styles.wrapper}>
-      <JobCard
-        vacancyTitle="Primary Level Teacher"
-        vacancyCompany="Gita Mata School"
-        location="Payutar, Kathmandu"
-        skills={["Communication", "Leadership","Something","Another","Next One","After One"]}
-        salaryMin="20000"
-        period="Full Time"
-        salaryMax="50000"
-        employmentType="On-site/In-Office"
-        level="Mid Level"
-        experienceRequired="More than or equal to 3yrs"
-        deadline="smth"
-        views="500"
-      />
-
+    <Tag icon= {<IconBills />} data="skill" layout="green"/>
+    <Tag icon= {<IconEyeOpen />} data="skill" layout="blue"/>
+    <Tag  data="skill" />
     </div>
   );
 }
