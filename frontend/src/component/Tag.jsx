ceefacf93387f180342bd82fae09d873362825d2
urekagautam <urekagautam19@gmail.com>
2025-05-21 12:12:31 +0545

    added color as one of the cns

--- frontend/src/component/Tag.jsx
+++ frontend/src/component/Tag.jsx
@@ -1,10 +1,10 @@
 import { cns } from '../utils/classNames'
 import styles from './Tag.module.css'
 
-export default function Tag({data, icon=null, layout="default"}) {
+export default function Tag({data, icon=null, layout="default", color="default"}) {
  
   return (
-    <div className={cns(styles.tagWrapper, styles[layout])}>
+    <div className={cns(styles.tagWrapper, styles[layout], styles[color])}>
       {icon}
       <h2>{data}</h2>
     </div>
