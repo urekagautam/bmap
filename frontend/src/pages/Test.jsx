2a4639eddafb7221ea37cbf0eb137ffe097cac56
urekagautam <urekagautam19@gmail.com>
2025-06-25 14:13:13 +0545

    created application card component

--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -1,16 +1,14 @@
 
+import ApplicationCard from "../component/ApplicationCard.jsx";
 import Tag from "../component/Tag.jsx";
 import styles from "./Test.module.css";
-import {IconEyeOpen} from "../component/icons/IconEyeOpen.jsx"
-import {IconBills} from "../component/icons/IconBills.jsx"
+
 
 
 export default function Test() {
   return (
     <div className={styles.wrapper}>
-    <Tag icon= {<IconBills />} data="skill" layout="green"/>
-    <Tag icon= {<IconEyeOpen />} data="skill" layout="blue"/>
-    <Tag  data="skill" />
+   <ApplicationCard />
     </div>
   );
 }
